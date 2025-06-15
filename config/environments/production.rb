5ccaaa802bed4e418e72c7771edccc589ffc9ed3
nach00 <bignacho@gmail.com>
2025-06-15 12:51:15 -0500

    Enable asset compilation in production and precompile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,6 +5,9 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
+  
+  # Skip asset precompilation errors
+  config.assets.compile = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
